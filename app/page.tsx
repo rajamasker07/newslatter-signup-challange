d335772a31c4d45681e4490f616a25dce05f16ac
rosyid <rajamasker@gmail.com>
2024-01-18 21:18:15 +0900

    add link to subscribe page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import Form from "./components/form";
+'use client';
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router);
+  }, [router]);
+
   return (
     <main className="flex min-h-screen w-screen items-center p-0">
       <div className="w-[500px] bg-white p-5">
-        <Form/>
+        <Link href={{ pathname: '/subscribe' }}> Go to subscribe page </Link>
       </div>
     </main>
   );
